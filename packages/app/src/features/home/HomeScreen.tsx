a0599508e0413b2543066a803aca656d8019c41a
kzlar <meata.storage@gmail.com>
2022-12-13 00:28:05 +0000

    enter style tests

--- packages/app/src/features/home/HomeScreen.tsx
+++ packages/app/src/features/home/HomeScreen.tsx
@@ -1,6 +1,6 @@
-import {Anchor, Button, H1, LmButton, Paragraph, Separator, XStack, YStack} from 'tamagui-extras'
+import {Anchor, Button, H1, Heading, LmButton, Paragraph, Separator, TextProps, useDidFinishSSR, XStack, YStack} from 'tamagui-extras'
 import {ChevronDown} from '@tamagui/lucide-icons'
-import React from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import {useLink} from 'solito/link'
 import {LmThemeToggle} from "app/src/components/various/LmThemeToggle";
 import {useToggleMainDrawer} from "app/src/state/appState";
@@ -20,6 +20,15 @@ export function HomeScreen() {
     return (
         <YStack flex={1} justifyContent="center" alignItems="center" padding="$4" space>
             <YStack space="$4" maxWidth={600}>
+            <AnimatedHeading
+                  key={`1`}
+                  index={10}
+                  Component={H1}
+                  family={'body'}
+                  color="$pink10"
+                >
+                  Swappable
+                </AnimatedHeading>
                 <H1 textAlign="center">Welcome to Tamagui.</H1>
                 <Separator/>
                 <Paragraph textAlign="center" size={'$2'}>
@@ -64,3 +73,58 @@ function SheetDemo() {
         </>
     )
 }
+
+
+
+
+const AnimatedHeading = memo(
+    ({
+      Component,
+      children,
+      family,
+      index,
+      ...rest
+    }: {
+      family: string
+      Component: typeof Heading
+      children: any
+      index: number
+    } & TextProps) => {
+      return (
+        // <Delay by={index * 180}>
+          <Component
+            animation="lazy"
+            enterStyle={{ o: 0}}
+            exitStyle={{ o: 0 }}
+            o={1}
+            pr="$1"
+            my="$1"
+            $sm={{
+              pr: 0,
+            }}
+            fontFamily={`$${family}`}
+            textShadowColor="$shadowColorFocus"
+            textShadowRadius={3}
+            textShadowOffset={{ width: 0, height: 3 }}
+            ellipse
+            {...rest}
+          >
+            {children}
+          </Component>
+        // </Delay>
+      )
+    }
+  )
+  
+  const Delay = ({ children, by }) => {
+    const isMounted = useDidFinishSSR()
+    const [done, setDone] = useState(false)
+  
+    useEffect(() => {
+      const showTimer = setTimeout(() => setDone(true), by)
+      return () => clearTimeout(showTimer)
+    })
+  
+    return !isMounted || !done ? null : children
+  }
+  
